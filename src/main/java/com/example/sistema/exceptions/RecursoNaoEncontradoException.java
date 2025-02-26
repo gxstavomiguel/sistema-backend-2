9a9dc53c7804947f31229f68c894e3b75bbfa802
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2025-02-26 02:39:38 -0300

    Nova versão

--- src/main/java/com/example/sistema/exceptions/RecursoNaoEncontradoException.java
+++
@@ -1,9 +0,0 @@
-package com.example.sistema.exceptions;
-
-public class RecursoNaoEncontradoException extends RuntimeException {
-
-    public RecursoNaoEncontradoException(String msg) {
-        super(msg);
-    }
-
-}
